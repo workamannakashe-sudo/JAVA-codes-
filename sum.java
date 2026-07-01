a34a315e310e2dcf96e283bf6cfcb91ad186ff0d
workamannakashe-sudo <work.amannakashe@gmail.com>
2026-07-01 11:05:56 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class main {
+
+public static void main(String [] args){
+
+
+   Scanner sc = new Scanner(System.in);
+   
+   System.out.println("Enter value of n1,n2");
+   int n1 = sc.nextInt();
+   
+   int n2 = sc.nextInt();
+   
+   System.out.println("ADD:"+ (n1+n2));
+   
+   }
+}
+
